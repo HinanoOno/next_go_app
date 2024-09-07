33df9acfcd26c66cd634bd04d494f35bc45a99db
HinanoOno <hinagon1231@icloud.com>
2024-09-07 23:39:18 +0900

    tesseractの導入

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -3,10 +3,11 @@
 import { useRef, useState } from "react";
 import Webcam from "react-webcam";
 import Image from "next/image";
+import { createWorker } from "tesseract.js";
 
 const videoConstraints = {
   width: 500,
-  height: 500,
+  height: 300,
   facingMode: "user",
 };
 
@@ -19,26 +20,42 @@ const Page = () => {
     setUrl(imageSrc);
   };
 
+  //画像読み取り
+  const readImage = async(url: string | null) => {
+    if (url) {
+      const worker = await createWorker('jpn');
+      const ret = await worker.recognize(url);
+      await worker.setParameters({
+        tessedit_char_blacklist: '賞味期限',
+      });
+      console.log(ret.data.text);
+      await worker.terminate();
+    }
+  }
+
   return (
     <div>
       <Webcam
         audio={false}
         width={500}
-        height={500}
+        height={300}
         ref={webcamRef}
-        mirrored={true}
+        mirrored={false}
         screenshotFormat="image/jpeg"
         videoConstraints={videoConstraints}
       />
       <button onClick={capture}>撮影</button>
-      {url && (
+      {url ? (
         <Image
           src={url}
           width={500}
-          height={500}
+          height={300}
           alt="description of the image"
         />
+      ):(
+        <div>画像がありません</div>
       )}
+      <button onClick={() => readImage(url)}>表示</button>
     </div>
   );
 };
